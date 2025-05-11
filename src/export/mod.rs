0b806c52454874a3b060e545102284774eb984ea
Brandon Essex <essex.brandon@gmail.com>
2025-05-10 20:33:59 -0400

    v2.1.0-beta4

--- src/export/mod.rs
+++ src/export/mod.rs
@@ -5,5 +5,6 @@ use std::io::Write;
 pub fn write_export_summary(summary: &ExportSummary, path: &str) -> std::io::Result<()> {
 let content = serde_json::to_string_pretty(summary)?;
 let mut file = File::create(path)?;
-file.write_all(content.as_bytes())?
+file.write_all(content.as_bytes())?;
+Ok(())
 }
