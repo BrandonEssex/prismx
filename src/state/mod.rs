de5dd90860239a62f554c100cc4731703ac5a220
Brandon Essex <17153818+BrandonEssex@users.noreply.github.com>
2025-05-25 01:43:07 -0400

    Add debug logging and fallback layout safeguards

--- src/state/mod.rs
+++ src/state/mod.rs
@@ -289,12 +289,13 @@ impl AppState {
         }
         if self.debug_input_mode {
             eprintln!(
-                "[Node {}] label=\"{}\", parent={:?}, x={}, y={}",
+                "[INSERT] Node {} \u{2192} label=\"{}\", parent={:?}, x={}, y={}, mode={:?}",
                 new_id,
                 child.label,
                 child.parent,
                 child.x,
-                child.y
+                child.y,
+                self.mode
             );
         }
 
@@ -352,12 +353,13 @@ impl AppState {
 
         if self.debug_input_mode {
             eprintln!(
-                "[Node {}] label=\"{}\", parent={:?}, x={}, y={}",
+                "[INSERT] Node {} \u{2192} label=\"{}\", parent={:?}, x={}, y={}, mode={:?}",
                 new_id,
                 sibling.label,
                 sibling.parent,
                 sibling.x,
-                sibling.y
+                sibling.y,
+                self.mode
             );
         }
 
